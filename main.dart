db0819b250de114558b4bbc01f682e7384029ac1
HarunYildiz <yildizharunn@gmail.com>
2023-07-16 22:43:13 +0300

    main.dart değiştirildi

--- main.dart
+++ main.dart
@@ -1,58 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(CalculatorApp());
+  runApp(const HesapMakinApp());
 }
 
-class CalculatorApp extends StatelessWidget {
+class HesapMakinApp extends StatelessWidget {
+  const HesapMakinApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
+      title: 'HesapMakin',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: CalculatorScreen(),
+      home: const CalculatorScreen(),
     );
   }
 }
 
 class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _CalculatorScreenState createState() => _CalculatorScreenState();
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
   double num1 = 0.0;
   double num2 = 0.0;
-  double result = 0.0;
+  double sonuc = 0.0;
   String operation = '';
 
   void calculateResult() {
     setState(() {
       switch (operation) {
         case '+':
-          result = num1 + num2;
+          sonuc = num1 + num2;
           break;
         case '-':
-          result = num1 - num2;
+          sonuc = num1 - num2;
           break;
         case '*':
-          result = num1 * num2;
+          sonuc = num1 * num2;
           break;
         case '/':
           if (num2 != 0) {
-            result = num1 / num2;
+            sonuc = num1 / num2;
           } else {
             showDialog(
               context: context,
               builder: (BuildContext context) {
                 return AlertDialog(
-                  title: Text('Error'),
-                  content: Text('Cannot divide by zero.'),
+                  title: const Text('Error'),
+                  content: const Text('Cannot divide by zero.'),
                   actions: <Widget>[
                     TextButton(
-                      child: Text('OK'),
+                      child: const Text('OK'),
                       onPressed: () {
                         Navigator.of(context).pop();
                       },
@@ -71,34 +76,33 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculator'),
+        title: const Text('Hesap Makinem - AMANE'),
       ),
       body: Container(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Result: $result',
-              style: TextStyle(fontSize: 20.0),
+              'Sonuç: $sonuc',
+              style: const TextStyle(fontSize: 20.0),
             ),
-            SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
             TextField(
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Enter the first number'),
+              decoration: const InputDecoration(labelText: 'İLK SAYIYI YAZ'),
               onChanged: (value) {
                 num1 = double.parse(value);
               },
             ),
             TextField(
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Enter the second number'),
+              decoration: const InputDecoration(labelText: 'İKİNCİ SAYIYI YAZ'),
               onChanged: (value) {
                 num2 = double.parse(value);
               },
             ),
-            SizedBox(height: 16.0),
-
+            const SizedBox(height: 16.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
@@ -107,28 +111,28 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                     operation = '+';
                     calculateResult();
                   },
-                  child: Text('+'),
+                  child: const Text('+'),
                 ),
                 ElevatedButton(
+                  child: const Text('-'),
                   onPressed: () {
                     operation = '-';
                     calculateResult();
                   },
-                  child: Text('-'),
                 ),
                 ElevatedButton(
+                  child: const Text('*'),
                   onPressed: () {
                     operation = '*';
                     calculateResult();
                   },
-                  child: Text('*'),
                 ),
                 ElevatedButton(
+                  child: const Text('/'),
                   onPressed: () {
                     operation = '/';
                     calculateResult();
                   },
-                  child: Text('/'),
                 ),
               ],
             ),
